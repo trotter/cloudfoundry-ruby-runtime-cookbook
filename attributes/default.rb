6a362aa564f5e0eadb604414e7e583dcf48e0e09
Trotter Cashion <cashion@gmail.com>
2012-04-02 22:23:29 -0700

    Use correct ruby18 version

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default[:cloudfoundry_ruby_runtime][:ruby_1_9_2_version] = "1.9.2-p290"
 default[:cloudfoundry_dea][:runtimes][:ruby18][:name]         = "ruby18"
 default[:cloudfoundry_dea][:runtimes][:ruby18][:cookbook]     = "cloudfoundry-ruby-runtime::ruby_1_8_7"
 default[:cloudfoundry_dea][:runtimes][:ruby18][:executable]   = File.join(node[:rbenv][:system_prefix], "rbenv", "versions", node[:cloudfoundry_ruby_runtime][:ruby_1_8_7_version], "bin", "ruby")
-default[:cloudfoundry_dea][:runtimes][:ruby18][:version]      = node[:cloudfoundry_ruby_runtime][:ruby_1_8_7_version].sub('-', '').sub(/p.*$/, '')
+default[:cloudfoundry_dea][:runtimes][:ruby18][:version]      = node[:cloudfoundry_ruby_runtime][:ruby_1_8_7_version].sub('-', '')
 default[:cloudfoundry_dea][:runtimes][:ruby18][:version_flag] = "-v | cut -d' ' -f2"
 default[:cloudfoundry_dea][:runtimes][:ruby18][:default]      = false
 
